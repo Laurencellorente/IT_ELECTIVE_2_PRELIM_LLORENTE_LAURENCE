577746f5c49cbb0c8132369d79728d6cf31fe833
Ninism <nin.alamo@outlook.com>
2026-07-10 16:23:31 +0800

    Update GetMealById.cs
    Signed-off-by: Ninism <nin.alamo@outlook.com>

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetMealById.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetMealById.cs
@@ -8,13 +8,13 @@ namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 // 2. Assert status code is 200 OK
 // 3. Parse the JSON and assert the meal name is "Arrabiata"
 //
-// Note: TheMealDB meal IDs are numeric (52772 = Arrabiata)
+// Note: TheMealDB meal IDs are numeric (52771 = Arrabiata)
 
 public static class GetMealById
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/lookup.php?i=52772
+        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/lookup.php?i=52771
         // TODO: Assert status code is 200 OK
         // TODO: Parse the response JSON
         // TODO: Assert the meal name (strMeal) is "Arrabiata"
