caff6255bf0ca516d0b6275c6a13fce8d934a311
Llorente <STUDENT@DESKTOP-TKVGKII>
2026-07-11 13:15:25 +0800

    getMealbyid done

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetMealById.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetMealById.cs
@@ -1,24 +1,34 @@
-namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
+using System.Text.Json;
 
-// EXERCISE 3: GET Lookup by ID
-// TheMealDB API: https://themealdb.com/api/json/v1/1/lookup.php?i={id}
-//
-// Your task:
-// 1. Use the HttpClient to look up meal with ID 52772
-// 2. Assert status code is 200 OK
-// 3. Parse the JSON and assert the meal name is "Arrabiata"
-//
-// Note: TheMealDB meal IDs are numeric (52771 = Arrabiata)
+namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 public static class GetMealById
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/lookup.php?i=52771
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Assert the meal name (strMeal) is "Arrabiata"
+        string url = "https://themealdb.com/api/json/v1/1/lookup.php?i=52772";
 
-        throw new NotImplementedException();
+        // Send GET request
+        var response = await client.GetAsync(url);
+
+        // Check status code
+        if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            throw new Exception("Status code is not 200 OK.");
+
+        // Read response
+        string body = await response.Content.ReadAsStringAsync();
+
+        // Parse JSON
+        using JsonDocument document = JsonDocument.Parse(body);
+
+        JsonElement meals = document.RootElement.GetProperty("meals");
+
+        if (meals.ValueKind == JsonValueKind.Null || meals.GetArrayLength() == 0)
+            throw new Exception("Meal not found.");
+
+        string mealName = meals[0].GetProperty("strMeal").GetString()!;
+
+        if (mealName != "Arrabiata")
+            throw new Exception($"Expected 'Arrabiata' but got '{mealName}'.");
     }
 }
